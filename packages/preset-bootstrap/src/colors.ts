b91c042727818f56290779cface299bd1256b8ce
Ty Mick <ty@tymick.me>
2021-12-03 08:53:02 -0800

    Update base colors to Bootstrap 5 values

--- packages/preset-bootstrap/src/colors.ts
+++ packages/preset-bootstrap/src/colors.ts
@@ -13,16 +13,16 @@ export const baseColors = {
     '#343a40',
     '#212529',
   ],
-  blue: '#007bff',
+  blue: '#0d6efd',
   indigo: '#6610f2',
   purple: '#6f42c1',
-  pink: '#e83e8c',
+  pink: '#d63384',
   red: '#dc3545',
   orange: '#fd7e14',
   yellow: '#ffc107',
-  green: '#28a745',
+  green: '#198754',
   teal: '#20c997',
-  cyan: '#17a2b8',
+  cyan: '#0dcaf0',
 }
 
 export const colors = {
