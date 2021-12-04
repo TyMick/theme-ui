b075151a9228a6b7566febc49f70c25c8faa1b4b
Ty Mick <ty@tymick.me>
2021-12-04 09:09:48 -0800

    Refactor color objects
    I'll soon need to reference `themeColors` in `themeUiColors`, so I at
    least need to separate those.
    
    Also, I realized that updating colors to Bootstrap 5 values in b91c042
    and 74d5284 is a breaking change anyway, so I've renamed `gray` to
    `grays` and defined a new `gray` as `grays[6]`.

--- packages/preset-bootstrap/src/colors.ts
+++ packages/preset-bootstrap/src/colors.ts
@@ -1,18 +1,20 @@
-export const baseColors = {
-  white: '#fff',
-  black: '#000',
-  gray: [
-    '#fff', // 0 index
-    '#f8f9fa',
-    '#e9ecef',
-    '#dee2e6',
-    '#ced4da',
-    '#adb5bd',
-    '#6c757d', // gray[6] is Bootstrap's primary $gray
-    '#495057',
-    '#343a40',
-    '#212529',
-  ],
+const white = '#fff'
+const grays = [
+  white, // 0 index
+  '#f8f9fa',
+  '#e9ecef',
+  '#dee2e6',
+  '#ced4da',
+  '#adb5bd',
+  '#6c757d',
+  '#495057',
+  '#343a40',
+  '#212529',
+]
+const black = '#000'
+
+// $colors
+const baseColors = {
   blue: '#0d6efd',
   indigo: '#6610f2',
   purple: '#6f42c1',
@@ -23,21 +25,34 @@ export const baseColors = {
   green: '#198754',
   teal: '#20c997',
   cyan: '#0dcaf0',
+  white,
+  gray: grays[6],
+  grayDark: grays[8],
 }
 
-export const colors = {
-  ...baseColors,
-  grayDark: baseColors.gray[8],
+// $theme-colors
+const themeColors = {
   primary: baseColors.blue,
-  secondary: baseColors.gray[6],
+  secondary: grays[6],
   success: baseColors.green,
   info: baseColors.cyan,
   warning: baseColors.yellow,
   danger: baseColors.red,
-  light: baseColors.gray[1],
-  dark: baseColors.gray[9],
-  text: baseColors.gray[9],
+  light: grays[1],
+  dark: grays[9],
+}
+
+const themeUiColors = {
+  text: grays[9],
   background: baseColors.white,
-  muted: baseColors.gray[3],
-  textMuted: baseColors.gray[6],
+  muted: grays[3],
+  textMuted: grays[6],
+}
+
+export const colors = {
+  grays,
+  black,
+  ...baseColors,
+  ...themeColors,
+  ...themeUiColors,
 }
